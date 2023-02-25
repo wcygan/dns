f55a4902f908668652152d2b515d0dc0e2a284c3
Will Cygan <wcygan.io@gmail.com>
2023-02-24 20:42:08 -0800

    UDP client and server

--- dns-client/src/main.rs
+++ dns-client/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use tokio::net::UdpSocket;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let socket = UdpSocket::bind("0.0.0.0:8081").await?;
+    let _ = socket.send_to(b"wcygan.io", "0.0.0.0:8080").await?;
+    Ok(())
 }
--- dns-server/src/main.rs
+++ dns-server/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use tokio::net::UdpSocket;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    loop {
+        let mut buf = [0; 512];
+        let socket = UdpSocket::bind("0.0.0.0:8080").await?;
+
+        let (size, src) = socket.recv_from(&mut buf).await?;
+        println!("{}: {}", src, String::from_utf8_lossy(&buf[..size]),);
+    }
 }
